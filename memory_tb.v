305dfadc1c7bcd8b452b5ab90ead5aff2bbeefe4
yiduoliu <3205516076@qq.com>
2025-01-21 15:27:23 +0800

    正在验证memory模块

--- memory_tb.v
+++ memory_tb.v
@@ -12,7 +12,7 @@ module memory_tb;
     wire dmem_error_o;   // 输出信号为 wire 类型
 
     // 实例化 memory_access 模块
-    memory memory_access(
+    memory_access memory_module(
         .clk_i(clk_i),
         .icode_i(icode_i),
         .valA_i(valA_i),
@@ -25,32 +25,135 @@ module memory_tb;
     // 初始化时，模拟 RAM 的值
     integer i;
     initial begin
-        for(i = 0; i < 255; i = i + 1) begin
-            memory_access.rams[i] = i;  // 将内存设置为某些值
+        for(i = 0; i < 1023; i = i + 1) begin
+            $display("write data to rams...addr=%h,num=%h",i,i);
+            memory_module.rams.rams[i] = i;  // 将内存设置为某些值
         end
     end
 
     // 设置输入信号
     initial begin
-        valA_i = 64'd8;
-        valE_i = 64'd16;
-        valP_i = 64'd32;
-        #5
+        #100
+        valA_i = 64'HA;
+        valE_i = 64'HE;
+        valP_i = 64'H2;
+        
+        $display("------------1st instruction rmmov: ----------------");
+        $display("M[valE] = valA_o");
+        $display("valA_o = %h", valA_i);
+        $display("valE_o = %h", valE_i);
+        $display("M[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
         icode_i = `IRMMOVQ;  // 设定指令
-        $display("valM_o = %h", valM_o);  // 显示 valM_o 的值
-        #10;
+        #2;
+        $display("------------EXECUTE-------");
+        //$display("valM_o = %h", valM_o);
+        $display("M[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        //$display("valA_o = %h", valA_i);
+        $display("------------Finesh--------------------------------------");
+        #2;
+
+        $display("------------2nd instruction mrmov: ----------------");
+        $display("valM=M[valE]");
+        $display("valM_o = %h", valM_o);
+        $display("valE_i = %h", valE_i);
+        $display("M[valE]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
+        icode_i = `IMRMOVQ;  // 设定指令
+        #2;
+        $display("------------EXECUTE-------");
+        //$display("valM_o = %h", valM_o);
+        //$display("rams[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        $display("valM = %h", valM_o);
+        $display("------------Finesh--------------------------------------");
+        #2;
+
+        $display("------------3rd instruction pushq: ----------------");
+        $display("M[valE] = valA_i");
+        $display("valA_i = %h", valA_i);
+        $display("valE_i = %h", valE_i);
+        $display("M[valE]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
+        icode_i = `IPUSHQ;  // 设定指令
+        #2;
+        $display("------------EXECUTE-------");
+        $display("M[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        //$display("valM = %h", valM_o);
+        $display("------------Finesh--------------------------------------");
+        #2;
+
+
+        $display("------------4th instruction popq: ----------------");
+        $display("valM=M[valA]");
+        $display("valM_i = %h", valM_o);
+        $display("valA_i = %h", valA_i);
+        $display("M[valE]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
+        icode_i = `IPOPQ;  // 设定指令
+        #2;
+        $display("------------EXECUTE-------");
+        //$display("rams[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        $display("valM = %h", valM_o);
+        $display("------------Finesh--------------------------------------");
+        #2;
+
+
+        $display("------------5th instruction popq: ----------------");
+        $display("valM=M[valA]");
+        $display("valM_i = %h", valM_o);
+        $display("valA_i = %h", valA_i);
+        $display("M[valE]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
+        icode_i = `IPOPQ;  // 设定指令
+        #2;
+        $display("------------EXECUTE-------");
+        //$display("rams[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        $display("valM = %h", valM_o);
+        $display("------------Finesh--------------------------------------");
+        #2;
+        
+
+        $display("------------6th instruction CALL: ----------------");
+        $display("M[valE] = valP");
+        $display("valP = %h", valP_i);
+        $display("valE_i = %h", valE_i);
+        $display("rams[valE]= %h", memory_module.rams.rams[valE_i]);
+        
+        #2;
+        icode_i = `ICALL;  // 设定指令
+        #2;
+        $display("------------EXECUTE-------");
+        //$display("rams[valE_o]= %h", memory_module.rams.rams[valE_i]);
+        $display("M[valE] = %h", memory_module.rams.rams[valE_i]);
+        $display("------------Finesh--------------------------------------");
+        #2;
+
+
+        #2;
     end
+
+
+
+    // // 初始化时，模拟 RAM 的值
+    // integer i;
+    // initial begin
+    //     for(i = 0; i < 1023; i = i + 1) begin
+    //         memory_access.rams[i] = i;  // 将内存设置为某些值
+    //     end
+    // end
+
+
+
 
     // 时钟生成
     initial begin
         clk_i = 0;
         forever #1 clk_i = ~clk_i;  // 时钟周期为 10ns
-    end
-
-    // 启动仿真
-    initial begin
-        clk_i = 0;
-        #5; // 等待初始时钟脉冲
     end
 
 endmodule
--- ram_tb.v
+++ ram_tb.v
@@ -19,48 +19,64 @@ module ram_tb;
         .dmem_error_o(dmem_error_o)
     );
 
+    // integer i;
+    // integer j;
+    // // 写操作控制
+    // initial begin 
+    //     // 初始化
+    //     write_en = 0;
+    //     read_en = 0;
+    //     addr_i = 0;
+    //     write_data_i = {64{1'b0}};  // 初始写入数据为 0
+
+    //     // 写入数据到RAM
+    //     for(j = 0; j < 255; j = j + 8) begin
+    //         addr_i = j;
+    //         write_data_i =j;//{64{1'b1}}; // 写入64个1
+    //         $display($time, ", write data to RAM: data is %h, addr is %h", write_data_i, addr_i);
+    //         write_en = 1;  // 使能写操作
+    //         #2 write_en = 0;  // 写操作结束后禁用写使能
+    //         #2;  // 确保信号稳定
+    //     end
+    // end
+
+    // // 读操作控制
+    // initial begin
+    //     #130
+    //     // 初始化读操作
+    //     read_en = 0;
+    //     addr_i = 0;
+
+    //     // 读取数据
+    //     for(i = 0; i < 255; i = i + 8) begin
+    //         addr_i = i;
+    //         read_en = 1;  // 使能读操作
+    //         #2 //,read_en = 0;  // 读取后禁用读使能
+    //         $display($time, ", read data from RAM: data is %h, addr is %h", read_data_o, addr_i);
+    //         #2;  // 确保信号稳定
+    //         read_en = 0; 
+    //     end
+    // end
+
+    // // 时钟生成
+    // initial begin
+    //     clk_i = 0;
+    //     forever #1 clk_i = ~clk_i;  // 时钟周期为 10ns
+    // end
+
+
     integer i;
-integer j;
-    // 写操作控制
-    initial begin 
-        // 初始化
-        write_en = 0;
-        read_en = 0;
-        addr_i = 0;
-        write_data_i = {64{1'b0}};  // 初始写入数据为 0
-
-        // 写入数据到RAM
-        for(j = 0; j < 255; j = j + 8) begin
-            addr_i = j;
-            write_data_i =j;//{64{1'b1}}; // 写入64个1
-            $display($time, ", write data to RAM: data is %h, addr is %h", write_data_i, addr_i);
-            write_en = 1;  // 使能写操作
-            #2 write_en = 0;  // 写操作结束后禁用写使能
-            #2;  // 确保信号稳定
+    initial begin
+        for(i = 0; i < 63; i = i + 1) begin
+            $display("write data to rams...addr=%h,num=%h",i,i);
+            ram_module.rams[i] = i;  // 将内存设置为某些值
         end
     end
-
-    // 读操作控制
     initial begin
-        #130
-        // 初始化读操作
-        read_en = 0;
-        addr_i = 0;
-
-        // 读取数据
-        for(i = 0; i < 255; i = i + 8) begin
-            addr_i = i;
-            read_en = 1;  // 使能读操作
-            #2 read_en = 0;  // 读取后禁用读使能
-            $display($time, ", read data from RAM: data is %h, addr is %h", read_data_o, addr_i);
-            #2;  // 确保信号稳定
+        #10 ;
+        for(i = 0; i < 63; i = i + 1) begin
+            $display("read data to rams...addr=%h,num=%h",i,ram_module.rams[i]);
         end
-    end
-
-    // 时钟生成
-    initial begin
-        clk_i = 0;
-        forever #1 clk_i = ~clk_i;  // 时钟周期为 10ns
     end
 
 endmodule
